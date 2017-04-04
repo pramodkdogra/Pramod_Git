449a275494613ae43edce22f6eb634ad29b0bdaa
pramodkdogra <p_dogra@hotmail.com>
2017-04-03 20:39:35 -0400

    Update Prepare_HDP_Env_CentOs.sh

--- Prepare_HDP_Env_CentOs.sh
+++ Prepare_HDP_Env_CentOs.sh
@@ -1,14 +1,16 @@
 ulimit -n 10000
 echo -e '\n \n' | ssh-keygen -t rsa
 chmod 700 ~/.ssh
-yum install -y ntp
+
 service ntpd start
 /etc/init.d/iptables stop
 setenforce 0
 echo umask 0022 >> /etc/profile
-yum install -y yum-utils createrepo
+
 mkdir -p /var/www/html/
 service ip6tables stop
 service sshd restart
 echo never > /sys/kernel/mm/redhat_transparent_hugepage/enabled
 echo never > /sys/kernel/mm/redhat_transparent_hugepage/defrag
+
+yum install -y epel=release yum-utils createrepo ntp pdsh pip
